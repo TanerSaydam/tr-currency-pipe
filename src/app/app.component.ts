9c491cf51f1a7cdfa17e84f609ddda5b92fe8756
tanersaydam <tanersaydam@gmail.com>
2022-09-08 22:56:48 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <input type="text" [(ngModel)]="number">
+  <h3 [innerText]="number | trCurrencyPipe"></h3>
+  `,
+  
 })
 export class AppComponent {
+  number: number = 5500;
   title = 'tr-currency-pipe';
 }
